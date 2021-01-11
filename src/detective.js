5dfd4753538624efbec6a0ce7a892dd3f797f50d
Sam Wong <theesamwong@gmail.com>
2021-01-10 21:16:29 -0800

    added detective to chase player

---
+++ src/detective.js
@@ -0,0 +1,51 @@
+class Detective {
+  constructor(ctx, canvas){
+    const character = new Image();
+    character.src = "../dist/images/L.png";
+    this.char = character;
+    this.following = true;
+    this.speed = 2;
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.radius = 40;
+    this.width = 32;
+    this.height = 48;
+    this.animateX = 0;
+    this.animateY = 0;
+    this.ctx = ctx;
+    this.canvas = canvas;
+  }
+
+  chasePlayer(playerX, playerY){
+    if(this.x > playerX && this.following){
+      this.animateY = 1
+      this.x -= this.speed;
+    }
+    if(this.x < playerX && this.following) {
+      this.animateY = 2;
+      this.x += this.speed;
+    }
+    if(this.y > playerY && this.following){
+      this.animateY = 3
+      this.y -= this.speed;
+    }
+    if(this.y < playerY && this.following){
+      this.animateY = 0;
+      this.y += this.speed;
+    }
+    if(this.y === playerY || this.x === playerX){
+      this.following = false;
+    }
+  }
+
+  movementFrames(){
+    if(this.animateX < 3 && this.following ){
+      this.animateX++
+    } else {
+      this.animateX = 0;
+    }
+  }
+
+}
+
+export default Detective;
--- src/index.js
+++ src/index.js
@@ -1,58 +1,18 @@
 import Player from './player';
+import Detective from './detective';
 const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 const player = new Player(ctx, canvas);
-// const keys = [];
-// const player = {
-//   moving: false,
-//   speed: 5,
-//   x: 400,
-//   y: 400,
-//   width: 32,
-//   height: 48,
-//   animateX: 0,
-//   animateY: 0,
-// }
+const playerX = player.x;
+const playerY = player.y;
+const detective = new Detective(ctx, canvas, playerX, playerY)
 
 canvas.width = 800;
 canvas.height = 500;
 
-// const character = new Image();
 const background = new Image();
-// character.src = "../dist/images/mandalorian2.png"
+
 background.src = "../dist/images/background.jpg";
-
-// const moveChar = () => {
-//   player.moving = false;
-//   if(keys['w'] && player.y > 50){
-//     player.y -= player.speed;
-//     player.animateY = 3;
-//     player.moving = true
-//   }
-//   if(keys['a'] && player.x > 0){
-//     player.x -= player.speed;
-//     player.animateY = 1;
-//     player.moving = true
-//   }
-//   if(keys['s'] && (canvas.height - 50) - player.height > player.y ){
-//     player.y += player.speed;
-//     player.animateY = 0;
-//     player.moving = true
-//   }
-//   if(keys['d'] && canvas.width - player.width > player.x){
-//     player.x += player.speed;
-//     player.animateY = 2;
-//     player.moving = true
-//   }
-// }
-
-// const movementFrames = () => {
-//   if(player.animateX < 3 && player.moving ) {
-//     player.animateX++
-//   } else {
-//     player.animateX = 0;
-//   }
-// }
 
 function drawCharacter(img, spriteX, spriteY, spriteW, spriteH, dX, dY, dW, dH){
   ctx.drawImage(img, spriteX, spriteY, spriteW, spriteH, dX, dY, dW, dH)
@@ -67,9 +27,12 @@ const animate = () => {
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
   drawCharacter(player.char, player.width * player.animateX, player.height * player.animateY,
                 player.width, player.height, player.x, player.y, player.width, player.height);
-  // moveChar();
+  drawCharacter(detective.char, detective.width * detective.animateX, detective.height * detective.animateY,
+                detective.width, detective.height, detective.x, detective.y, detective.width, detective.height);
   player.moveChar()
   player.movementFrames();
+  detective.chasePlayer(player.x, player.y);
+  detective.movementFrames();
 }
 
 window.addEventListener('keydown', e => {
