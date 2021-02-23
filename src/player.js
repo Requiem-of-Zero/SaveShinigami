57622b80c88d9dcd611851a442ca25c231951e8a
Sam Wong <theesamwong@gmail.com>
2021-02-23 12:04:11 -0800

    updated player to be used in game

--- src/player.js
+++ src/player.js
@@ -1,46 +1,48 @@
 class Player {
-  constructor(ctx, canvas){
+  constructor(dimensions){
     const character = new Image();
     character.src = "../dist/images/lightyagami.png";
     this.char = character;
+
     this.moving = false;
     this.speed = 5;
+    this.type = 'player'
+    this.dimensions = dimensions
     this.x = 400;
     this.y = 400;
-    this.radius = 40;
+
     this.width = 32;
     this.height = 48;
     this.animateX = 0;
     this.animateY = 0;
-    this.ctx = ctx;
+
     this.keys = [];
-    this.canvas = canvas;
   }
 
-  moveChar(){
+  moveChar(keys){
     this.movementFrames();
 
     this.moving = false;
-    if(this.keys['w'] && this.y > 50){
-    this.y -= this.speed;
-    this.animateY = 3;
-    this.moving = true
-  }
-  if(this.keys['a'] && this.x > 0){
-    this.x -= this.speed;
-    this.animateY = 1;
-    this.moving = true
-  }
-  if(this.keys['s'] && (this.canvas.height - 50) - this.height > this.y ){
-    this.y += this.speed;
-    this.animateY = 0;
-    this.moving = true
-  }
-  if(this.keys['d'] && this.canvas.width - this.width > this.x){
-    this.x += this.speed;
-    this.animateY = 2;
-    this.moving = true
-  }
+    if(keys['w'] && this.y > 50){
+      this.y -= this.speed;
+      this.animateY = 3;
+      this.moving = true
+    }
+    if(keys['a'] && this.x > 0){
+      this.x -= this.speed;
+      this.animateY = 1;
+      this.moving = true
+    }
+    if(keys['s'] && this.y < this.dimensions.height - this.height ){
+      this.y += this.speed;
+      this.animateY = 0;
+      this.moving = true
+    }
+    if(keys['d'] && this.x < this.dimensions.width - this.width){
+      this.x += this.speed;
+      this.animateY = 2;
+      this.moving = true
+    }
   }
 
   movementFrames(){
@@ -51,10 +53,15 @@ class Player {
     }
   }
 
-//   drawCharacter(img, spriteX, spriteY, spriteW, spriteH, dX, dY, dW, dH){
-//   this.ctx.drawImage(img, spriteX, spriteY, spriteW, spriteH, dX, dY, dW, dH)
-// }
-
+  animate(ctx) {
+    ctx.drawImage(this.char, 
+      this.width * this.animateX,
+      this.height * this.animateY,
+      this.width, this.height,
+      this.x, this.y,
+      this.width, this.height
+      );
+  }
 }
 
 export default Player;
